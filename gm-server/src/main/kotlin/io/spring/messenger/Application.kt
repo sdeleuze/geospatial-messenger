e213962d808939f53953c0141185a69bc5cb0300
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-03-23 21:37:30 +0100

    Unmock

--- gm-server/src/main/kotlin/io/spring/messenger/Application.kt
+++ gm-server/src/main/kotlin/io/spring/messenger/Application.kt
@@ -7,13 +7,14 @@ import io.spring.messenger.domain.Message
 import io.spring.messenger.domain.User
 import io.spring.messenger.repository.MessageRepository
 import io.spring.messenger.repository.UserRepository
-import org.postgis.Point
+import org.jetbrains.exposed.sql.Database
 import org.postgis.geojson.PostGISModule
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import javax.sql.DataSource
 
 @SpringBootApplication
 open class Application {
@@ -24,19 +25,21 @@ open class Application {
         return mapper
     }
 
-    @Bean open fun init(ur: UserRepository, mr: MessageRepository) = CommandLineRunner {
+    @Bean open fun db(dataSource: DataSource) = Database.connect(dataSource)
+
+    @Bean open fun init(db: Database, ur: UserRepository, mr: MessageRepository) = CommandLineRunner {
+        ur.createTable()
+        mr.createTable()
+        mr.deleteAll()
+        ur.deleteAll()
+
         ur.create(User("swhite", "Skyler", "White"))
         ur.create(User("jpinkman", "Jesse", "Pinkman"))
         ur.create(User("wwhite", "Walter", "White"))
         ur.create(User("sgoodman", "Saul", "Goodman"))
-        mr.create(Message("This is a test!", "swhite", Point(597737.56119008, 5833062.5024984), 1))
-        mr.create(Message("This is a test!", "swhite", Point(704137.90456304, 5740726.5723299), 2))
-        mr.create(Message("This is a test!", "swhite", Point(1643701.8562444, 6070934.5345218), 3))
-        mr.create(Message("This is a test!", "swhite", Point(1095801.2374963, 6648186.9721315), 4))
-        mr.create(Message("This is a test!", "swhite", Point(190786.8225998, 6276397.2665524), 5))
-        mr.create(Message("This is a test!", "swhite", Point(-391357.5848201, 7254791.2286026), 6))
-        mr.create(Message("This is a test!", "swhite", Point(-391357.5848201, 5258867.5460201), 6))
+        mr.create(Message("This is a test!", "swhite"))
     }
+
 }
 
 fun main(args: Array<String>) {
--- gm-server/src/main/kotlin/io/spring/messenger/repository/UserRepository.kt
+++ gm-server/src/main/kotlin/io/spring/messenger/repository/UserRepository.kt
@@ -1,21 +1,51 @@
 package io.spring.messenger.repository
 
+import io.spring.messenger.Users
 import io.spring.messenger.domain.User
+import io.spring.messenger.within
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import org.postgis.PGbox2d
 import org.postgis.Point
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
 @Repository
-open class UserRepository {
+open class UserRepository @Autowired constructor(val db: Database) {
 
-    val users = mutableListOf<User>()
+    open fun createTable() = db.transaction {
+        create(Users)
+    }
 
-    open fun create(user: User) = users.add(user)
+    open fun create(user: User) = db.transaction {
+            Users.insert( toRow(user) )
+    }
 
-    open fun updateLocation(userName:String, location: Point) { }
+    open fun updateLocation(userName:String, location: Point) = db.transaction {
+        location.srid = 4326
+        Users.update({Users.userName eq userName}) { it[Users.location] = location}
+    }
 
-    open fun findAll() = users
+    open fun findAll() = db.transaction {
+        Users.selectAll().map { fromRow(it) }
+    }
 
-    open fun findByBoundingBox(box: PGbox2d) = users
+    open fun findByBoundingBox(box: PGbox2d) = db.transaction {
+        Users.select { Users.location within box }.map { fromRow(it) }
+    }
+
+    open fun deleteAll() = db.transaction {
+        Users.deleteAll()
+    }
+
+    fun toRow(u: User): Users.(UpdateBuilder<*>) -> Unit = {
+        it[userName] = u.userName
+        it[firstName] = u.firstName
+        it[lastName] = u.lastName
+        it[location] = u.location
+    }
+
+    fun fromRow(r: ResultRow) =
+        User(r[Users.userName], r[Users.firstName], r[Users.lastName], r[Users.location])
 
 }
