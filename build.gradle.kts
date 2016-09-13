ad096dd187fd4648d03031206fe9bb37c9fd083e
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-09-13 10:43:20 +0200

    Migrate Gradle build file to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+buildscript {
+
+  extra["exposedVersion"] = "0.5.0"
+  extra["kotlinVersion"] = "1.0.4-eap-118"
+
+  repositories {
+    mavenCentral()
+    gradleScriptKotlin()
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+  }
+  dependencies {
+    classpath(kotlinModule("gradle-plugin", extra["kotlinVersion"]))
+    classpath("org.springframework.boot:spring-boot-gradle-plugin:1.4.0.RELEASE")
+  }
+}
+
+apply {
+  plugin("kotlin")
+  plugin("spring-boot")
+}
+
+repositories {
+  mavenCentral()
+  maven { setUrl("https://dl.bintray.com/kotlin/exposed") }
+  maven { setUrl("https://dl.bintray.com/sdeleuze/maven/") }
+  maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+}
+
+dependencies {
+
+  compile("org.springframework.boot:spring-boot-starter-web") {
+    exclude(module = "spring-boot-starter-validation")
+  }
+  compile("org.springframework.boot:spring-boot-starter-jdbc")
+  compile("org.springframework.boot:spring-boot-devtools")
+
+  compile(kotlinModule("stdlib", extra["kotlinVersion"]))
+  compile(kotlinModule("reflect", extra["kotlinVersion"]))
+  compile("io.projectreactor:reactor-core:3.0.0.RC1")
+  compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.7.5")
+
+  compile("org.jetbrains.exposed:exposed:${extra["exposedVersion"]}")
+  compile("org.jetbrains.exposed:spring-transaction:${extra["exposedVersion"]}")
+  compile("org.postgresql:postgresql:9.4.1208")
+  compile("net.postgis:postgis-jdbc:2.2.0") {
+    exclude(module = "postgresql")
+  }
+  compile("com.github.mayconbordin:postgis-geojson:1.1")  {
+    exclude(module = "postgresql")
+  }
+
+  testCompile("org.springframework.boot:spring-boot-starter-test")
+  testCompile("org.springframework.restdocs:spring-restdocs-mockmvc:1.1.1.RELEASE")
+}
