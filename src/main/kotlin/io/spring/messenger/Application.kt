d611290046462aa0f8c7489331f6dbf4cf8aa045
Mathias Düsterhöft <mduesterhoeft@epages.com>
2018-02-21 22:56:47 +0100

    remove config contained in spring-boot-starter-exposed

--- src/main/kotlin/io/spring/messenger/Application.kt
+++ src/main/kotlin/io/spring/messenger/Application.kt
@@ -2,11 +2,11 @@ package io.spring.messenger
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.md.exposed.ExposedTables
 import io.spring.messenger.domain.Message
 import io.spring.messenger.domain.User
 import io.spring.messenger.repository.MessageRepository
 import io.spring.messenger.repository.UserRepository
-import org.jetbrains.exposed.spring.SpringTransactionManager
 import org.postgis.geojson.PostGISModule
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.transaction.annotation.EnableTransactionManagement
-import javax.sql.DataSource
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -28,16 +27,16 @@ class Application {
         return mapper
     }
 
-    @Bean
-    fun transactionManager(dataSource: DataSource) = SpringTransactionManager(dataSource)
-
     @Bean // PersistenceExceptionTranslationPostProcessor with proxyTargetClass=false, see https://github.com/spring-projects/spring-boot/issues/1844
     fun persistenceExceptionTranslationPostProcessor() = PersistenceExceptionTranslationPostProcessor()
 
     @Bean
+    fun exposedTables(): ExposedTables {
+        return ExposedTables(listOf(Users, Messages))
+    }
+
+    @Bean
     fun init(ur: UserRepository, mr: MessageRepository) = CommandLineRunner {
-        ur.createTable()
-        mr.createTable()
         mr.deleteAll()
         ur.deleteAll()
 
